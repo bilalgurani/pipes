089efe103cf66942ca72f232171c24974f9250d8
BilalAhamedGurani <bilal.gurani@utopusinsights.com>
2022-11-18 12:23:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipe';
+  status = 'Stable';
+  filterStatus = '';
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Environment Server',
+      status: 'critical',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  getServerStatus(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-success': server.status === 'stable',
+      'list-warning': server.status === 'offline',
+      'list-danger': server.status === 'critical',
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
